a1b931a991b15e2f2a3428f2e7eed4097c6af5d6
reFucktor <hmvalcarcel@gmail.com>
2016-06-22 02:39:25 -0400

    Update models with facebook and google profiles.

--- app/backend/models.js
+++ app/backend/models.js
@@ -3,13 +3,36 @@
 const mongoose = require('mongoose'),
     Schema = mongoose.Schema;
 
+const FacebookUserSchema = new Schema({
+    id          : { type: String },
+    name        : { type: String },
+    email       : { type: String },
+    avatarUrl   : { type: String },
+    profileUrl  : { type: String },
+    gender      : { type: String }
+});
+
+const GoogleUserSchema = new Schema({
+    id          : { type: String },
+    name        : { type: String },
+    email       : { type: String },
+    avatarUrl   : { type: String },
+    gender      : { type: String },
+    profileUrl  : { type: String },
+    birthday    : { type: String }
+});
+
 const UserProfileSchema = new Schema({
     name            : { type: String },
     email           : { type: String },
     loginAvatarUrl  : { type: String },
-    regDate        : { type: Date, default: new Date()}
+    regDate         : { type: Date, default: new Date()},
+    facebookProfile : FacebookUserSchema,
+    googleProfile   : GoogleUserSchema
 });
 
 module.exports = {
-    UserProfile: mongoose.model('userProfile', UserProfileSchema)
+    UserProfile: mongoose.model('userProfile', UserProfileSchema),
+    FacebookProfile: mongoose.model('facebookProfile', FacebookUserSchema),
+    GoogleProfile: mongoose.model('googleProfile', GoogleUserSchema)
 };
