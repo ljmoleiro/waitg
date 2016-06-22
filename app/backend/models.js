d3b743c8b5583dfb876d8c5fd49cfa2b6464f2f6
reFucktor <hmvalcarcel@gmail.com>
2016-06-22 01:06:56 -0400

    Created model: UserProfile.

--- app/backend/models.js
+++ app/backend/models.js
@@ -1,3 +1,15 @@
-/**
- * Created by refucktor on 6/22/16.
- */
+'use strict';
+
+const mongoose = require('mongoose'),
+    Schema = mongoose.Schema;
+
+const UserProfileSchema = new Schema({
+    name            : { type: String },
+    email           : { type: String },
+    loginAvatarUrl  : { type: String },
+    regDate        : { type: Date, default: new Date()}
+});
+
+module.exports = {
+    UserProfile: mongoose.model('userProfile', UserProfileSchema)
+};
